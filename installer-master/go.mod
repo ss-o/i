50a65fed9afeb14113268d484c95bef849818a77
Salvydas Lukosius <sall@w-ss.io>
2021-09-14 10:08:54 +0100

    add

---
+++ installer-master/go.mod
@@ -0,0 +1,10 @@
+module github.com/ss-o/i
+
+go 1.14
+
+// +heroku goVersion go1.14
+
+require (
+	github.com/jpillora/opts v1.1.2
+	github.com/rakyll/statik v0.1.7
+)
---
+++ installer-master/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/jpillora/opts"
+	"github.com/ss-o/i/handler"
+)
+
+var version = "0.0.0-src"
+
+func main() {
+	c := handler.DefaultConfig
+	opts.New(&c).Repo("github.com/ss-o/i").Version(version).Parse()
+	log.Printf("Default user is '%s', GH token set: %v, listening on %d...", c.User, c.Token != "", c.Port)
+	h := &handler.Handler{Config: c}
+	if err := http.ListenAndServe(":"+strconv.Itoa(c.Port), h); err != nil {
+		log.Fatal(err)
+	}
+}
