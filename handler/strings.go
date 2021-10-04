306b2ab44dc826b9a0078006f01acc4ff1aa2d99
Salvydas Lukosius <sall@w-ss.io>
2021-10-04 18:07:36 +0100

    modified:   handler/strings.go 	modified:   scripts/statik.go

--- handler/strings.go
+++ handler/strings.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	archRe    = regexp.MustCompile(`(aarch64|armv6|armv7|armv8|arm64|arm|x32|amd32|x86(-|_)?32|386|x64|amd64|x86(-|_)?64|mips64le|mips64|mipsle|mips|ppc64le|ppc64|ppc|ppcle|s390x)`)
+	archRe    = regexp.MustCompile(`(aarch64|armv6|armv7|armv8|arm64|arm|amd32|x32|x86(-|_)?32|386|amd64|x64|x86(-|_)?64|mips64le|mips64|mipsle|mips|ppc64le|ppc64|ppc|ppcle|s390x)`)
 	fileExtRe = regexp.MustCompile(`(\.[a-z][a-z0-9]+)+$`)
 	posixOSRe = regexp.MustCompile(`(android|darwin|linux|(net|free|open)bsd|mac|osx|windows|win|solaris)`)
 )
